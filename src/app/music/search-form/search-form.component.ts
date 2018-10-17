26fc067adc8cc2ddf4ea0208265a68faf4154530
Grzegorz Kasperski <grzegorz.kasperski@globallogic.com>
2018-10-17 12:02:21 +0200

    implemented custom  validator

--- src/app/music/search-form/search-form.component.ts
+++ src/app/music/search-form/search-form.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { FormGroup, AbstractControl, FormControl, FormArray, FormBuilder } from '@angular/forms';
-import { filter, distinctUntilChanged, debounceTime } from 'rxjs/operators';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import {
+  FormGroup,
+  AbstractControl,
+  FormControl,
+  FormArray,
+  FormBuilder,
+  Validators,
+  ValidatorFn,
+  ValidationErrors
+} from "@angular/forms";
+import { filter, distinctUntilChanged, debounceTime } from "rxjs/operators";
+import { AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-search-form',
-  templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.scss']
+  selector: "app-search-form",
+  templateUrl: "./search-form.component.html",
+  styleUrls: ["./search-form.component.scss"]
 })
 export class SearchFormComponent implements OnInit {
-
   queryForm: FormGroup;
 
-  @Output() queryChange = new EventEmitter();
+  @Output()
+  queryChange = new EventEmitter();
 
   constructor(private builder: FormBuilder) {
+
+    const censor = (badword: string): ValidatorFn => (control: AbstractControl): ValidationErrors | null => {
+      const hasError = (control.value as string).includes('batman');
+      return hasError ? {
+        censor: {
+          badword
+        }
+      } : null;
+    } 
+
     this.queryForm = this.builder.group({
-      query: [],
+      query: ["", [Validators.required, Validators.minLength(3), censor('batman')]]
       // options: new FormGroup({
       //   type: new FormControl('new type'),
       //   markets: new FormArray([
@@ -25,22 +45,19 @@ export class SearchFormComponent implements OnInit {
       // })
     });
     console.log(this.queryForm);
-    const value$ = this.queryForm.get('query').valueChanges;
+    const value$ = this.queryForm.get("query").valueChanges;
     value$
-    .pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      filter(query => query.length >= 3),
-    )
-    .subscribe(query => {
-      this.search(query);
-    })
-   }
-
-
-  ngOnInit() {
-
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged(),
+        filter(query => query.length >= 3)
+      )
+      .subscribe(query => {
+        this.search(query);
+      });
   }
+
+  ngOnInit() {}
 
   search(query: string) {
     this.queryChange.emit(query);
